dfb066245f668dbf26d5cfd9338fcd954894c7d8
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-12-04 17:21:55 -0500

    login

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,32 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+
+    const user = await User.findOne({ where: { username } });
+    
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { username: user.username },
+      process.env.JWT_SECRET_KEY || "",
+      { expiresIn: "1h" } 
+    );
+
+    return res.status(200).json({ token });
+  
 };
 
 const router = Router();
