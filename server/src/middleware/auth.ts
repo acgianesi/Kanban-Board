82160cc70d7e1d39830f7621c43e492b90c1e988
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-12-04 16:31:23 -0500

    authenticate

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -4,7 +4,29 @@ import jwt from 'jsonwebtoken';
 interface JwtPayload {
   username: string;
 }
+// activity 25
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  
+  const authHeader = req.headers.authorization;
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    const secretKey = process.env.JWT_SECRET_KEY || "";
+   
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user as JwtPayload;
+      
+      return next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
 };
